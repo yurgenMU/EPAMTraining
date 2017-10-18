b328b2cc693513691eefaf67162d02b49bf66b16
Evgeniy Macsyom <yurgen-95@mail.ru>
2017-10-18 17:09:18 +0300

    Task3 in progress (subtask2 is almost finished)

---
+++ HomeTasks/src/main/java/hometask3/t02/Main.java
@@ -0,0 +1,50 @@
+package hometask3.t02;
+
+import javax.print.DocFlavor;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Locale russian = new Locale("ru");
+        Locale english = new Locale("eng");
+        Locale actucalLocale = null;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Choose language" + "\n" + "English" + "\n" + "Русский");
+            String f = scanner.nextLine();
+            if (f.equals("English")) {
+                System.out.println(getQuestionsList("questions", english));
+                actucalLocale = english;
+            } else if (f.equals("Русский")) {
+                System.out.println(getQuestionsList("questions", russian));
+                actucalLocale = russian;
+            }
+            while (true) {
+                String number = scanner.nextLine();
+                if (number.equals("Exit") || number.equals("Выход"))
+                    System.exit(0);
+                ResourceBundle resourceBundle = ResourceBundle.getBundle("answers", actucalLocale, new UTF8Control());
+                int bundleSize = resourceBundle.keySet().size() - 1;
+                if (Integer.parseInt(number) <= bundleSize)
+                    System.out.println(resourceBundle.getString(number));
+                else
+                    System.out.println(resourceBundle.getString(String.valueOf((resourceBundle.keySet().size()))));
+
+
+            }
+        }
+    }
+
+    public static String getQuestionsList(String baseName, Locale locale) {
+        ResourceBundle resourceBundle = ResourceBundle.getBundle(baseName, locale, new UTF8Control());
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : resourceBundle.keySet()) {
+            stringBuilder.append(s).append(".").append(resourceBundle.getString(s)).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+}
