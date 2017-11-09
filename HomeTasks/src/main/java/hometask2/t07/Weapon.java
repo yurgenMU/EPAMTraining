b897fa32fa57bb1117dad9082c61e6baecaadf9b
Evgeniy Macsyom <yurgen-95@mail.ru>
2017-11-09 23:30:09 +0300

    Annotation added

---
+++ HomeTasks/src/main/java/hometask2/t07/Weapon.java
@@ -0,0 +1,10 @@
+package hometask2.t07;
+
+import java.lang.annotation.*;
+
+@Target(value = ElementType.TYPE)
+@Retention(value = RetentionPolicy.RUNTIME)
+@Documented
+public @interface Weapon {
+
+}
